d7653a61c24d87998841b5f58100a3fd7b855fe8
Айдын Ибраев (Aydyn Ibraev) <AIbraev@alfabank.kz>
2020-11-01 22:21:32 +0600

    feat: component for country selection

--- src/components/country-select.tsx
+++ src/components/country-select.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators, Dispatch } from 'redux';
+import { getCurrencies } from '../redux/currencies/actionsCreators';
+import { CurrenciesState } from '../redux/currencies/types';
 
 type CountrySelectProps = {
   defaultValue: string;
+  getCurrencies: () => void;
+  state?: any;
 };
 
 const CountrySelect = ({
   defaultValue,
+  getCurrencies,
+  state,
 }: CountrySelectProps): React.ReactElement => {
+  console.log('state', state);
+  useEffect(() => {
+    getCurrencies();
+  }, []);
   return (
     <div>
       <select defaultValue={defaultValue} />
@@ -15,4 +26,14 @@ const CountrySelect = ({
   );
 };
 
-export default CountrySelect;
+const mapStateToProps = (state: CurrenciesState) => state;
+
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      getCurrencies,
+    },
+    dispatch
+  );
+
+export default connect(mapStateToProps, mapDispatchToProps)(CountrySelect);
